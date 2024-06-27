417bf2cea2f305dd0d4f722451c0a9772051458f
Evan Paterakis <evan@geopjr.dev>
2024-06-27 18:52:33 +0300

    step 1

--- src/window.cr
+++ src/window.cr
@@ -3,20 +3,20 @@ module Text::Viewer
     resource: "/com/example/TextViewer/text-viewer-window.ui",
     children: {
       "header_bar",
-      "label",
+      "main_text_view",
     }
   )]
   class Window < Adw::ApplicationWindow
     include Gtk::WidgetTemplate
 
     @header_bar = Adw::HeaderBar
-    @label : Gtk::Label
+    @main_text_view : Gtk::TextView
 
     def initialize
       super()
 
       @header_bar = Adw::HeaderBar.cast(template_child("header_bar"))
-      @label = Gtk::Label.cast(template_child("label"))
+      @main_text_view = Gtk::TextView.cast(template_child("main_text_view"))
     end
   end
 end
